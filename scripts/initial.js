bd57b1b12f7e5c16e3bf6e8ad91ef64edce95caf
cbril <cbril@csu.fullerton.edu>
2019-02-16 15:02:59 -0800

    Integrating user existence checks with profile page

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,40 +1,77 @@
+console.log("In initial.js");
 var API_BASE = "https://api.github.com";
 
+// var initial_users = [];
+// for(let i of [1,2,3,4]){
+//     let param = 'user' + i;
+//     let user = url.searchParams.get(param);
+//     if(user){
+//         initial_users.push(user);
+//     }
+// }
+
+var userCards = [];
+
+if (errs == 0 && users){
+    console.log(`\tChecking that all GitHub users exists`);
+    userCards = verifyUsers().then( (cards) => {
+        console.log(`\tCreated ${cards.length} cards`);
+    });
+}
+
 async function verifyUsers(){
-  var cards = [];
-    var textfield = document.getElementsByClassName("username");
-    for(var i = 0; i < textfield.length; i++){
-      if(textfield[i].value != ''){
-        var data = await checkUsername(textfield[i].value)
+    let cards = [];
+    for(var i = 0; i < users.length; i++){
+      if(users[i] != ''){
+        console.log(`\t\tChecking user ${users[i]}`);
+        var data = await checkUsername(users[i])
         if(data){
-          // console.log("Good User!");
           await cards.push(new userCard(data.login, data.email, data.avatar_url ));
-          
         }
       }
     }
-    console.log(cards);
+    return cards;
 }
-
-
 
 async function checkUsername(username){
   try {
     const API_ENDPOINT = `${API_BASE}/users/${username}`
-    console.log(API_ENDPOINT)
+    // console.log(API_ENDPOINT)
     const request = await fetch(API_ENDPOINT);
     const data = await request.json();
+    const status = await request.status;
+    if(status != 200){
+      throw {
+        statusCode: status,
+        userName: username
+      }
+    } else {
+      return data;
+    }
     return data;
   }
   catch (e) {
     console.log(e);
-    return false;
+    const detailError = `${e.statusCode} code received. ${e.userName} is an invalid github username`;
+    let html =
+    `<div id="error"
+          <body>
+              <h2 >Error</h2>
+              <p id="details">One or more of the GitHub usernames provided was invalid.</p>
+              <a href="index.html"><strong>Try again.</strong></a>
+          </body>
+      </div>`;
+    document.getElementById("submit-results").innerHTML = html;
+    let newDetail = document.createElement('p');
+    newDetail.textContent = detailError;
+    document.getElementById("details").append(newDetail);
+    errs += 1;
   }
 }
 
 class userCard {
   constructor(username, email, avatar_link){
-    console.log("Constructor called" + username);
+    console.log("\t\tConstructor called" + username);
     this.email = (email != null) ? email : null
     this.avatar = avatar_link;
     this.username = username;
