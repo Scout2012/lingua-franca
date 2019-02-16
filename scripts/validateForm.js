4c18bb157a3029cd685de8fb3bb2b24fae22f8a4
cbril <cbril@csu.fullerton.edu>
2019-02-16 11:06:29 -0800

    Checking for valid user parameters in profile.html

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,32 +1,34 @@
-// True if user did not enter any GitHub usernames on the Home Page
-function emptyForm(){
-    let usernames = document.getElementsByClassName('username');
-    if (usernames){
-        let inputExists = false;
-        for (let user of usernames){
-            if (user.value != ''){
-                inputExists = true;
-            }
-        }
-        
-        return !inputExists;
+// ========== VALIDATE FORM PARAMETERS ==========
 
-    } else{
-        // There's no 'username' class in the HTML! :-(
-        return true;
+// ---------- Fetch the user parameters passed through the form
+let urlString = window.location.href;
+let url = new URL(urlString);
+
+let users = [];
+for(let i of [1,2,3,4]){
+    let param = 'user' + i;
+    let user = url.searchParams.get(param);
+    if(user){
+        users.push(user);
     }
 }
+console.log("Retrieved form inputs: " + users);
 
-function validateForm(){
-    if(emptyForm()){
-        document.getElementById('error-message').innerHTML = '<p>Please enter at least one GitHub username</p>';
-    }
-
-    // perform the other checks here
-
-    // if all checks pass, we go to the profiles page
+if (users.length == 0){
+    // Error: User has not entered any usernames
+    let html = 
+    `
+        <body>
+            <h2>Error</h2>
+            <p>Please enter at least one valid GitHub username.</p>
+            <a href="index.html"><strong>Try again.</strong></a>
+        </body>
+    `;
+    document.getElementById('submit-results').innerHTML = html;
 
 }
 
-
-// ===== MAIN =====
+for(let user of users){
+    // Make sure each user exists
+    console.log("Checking user " + user + "...");
+}
