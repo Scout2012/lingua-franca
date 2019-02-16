73d7d91de6772e430f541b671399b3c7b7afd1a6
cbril <cbril@csu.fullerton.edu>
2019-02-16 13:10:04 -0800

    Dynamically display user information on profile.html

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,6 +1,8 @@
 // ========== VALIDATE FORM PARAMETERS ==========
 
 // ---------- Fetch the user parameters passed through the form
+
+let errorCase = false;
 let urlString = window.location.href;
 let url = new URL(urlString);
 
@@ -15,20 +17,50 @@ for(let i of [1,2,3,4]){
 console.log("Retrieved form inputs: " + users);
 
 if (users.length == 0){
-    // Error: User has not entered any usernames
+    console.log("ERROR: no usernames were passed");
+
     let html = 
-    `
+    `<div id="error"
         <body>
             <h2>Error</h2>
             <p>Please enter at least one valid GitHub username.</p>
             <a href="index.html"><strong>Try again.</strong></a>
         </body>
-    `;
+    </div>`;
     document.getElementById('submit-results').innerHTML = html;
-
+    errorCase = true;
 }
 
+console.log("Checking for non-existing users");
 for(let user of users){
-    // Make sure each user exists
-    console.log("Checking user " + user + "...");
+    console.log(`\tVerifying user ${user}`);
+
 }
+
+if (!errorCase){
+    console.log("Generating HTML for each user");
+
+    let i = 0;
+    while(i < users.length){
+        let user = users[i];
+        console.log(`\tUser: ${user}`);
+        let id = 'user' + (i+1);
+        console.log(`\tHTML id: ${id}`);
+        let avatarSrc = "https://avatars3.githubusercontent.com/u/32445362?v=4";
+        console.log(`\tAvatar: ${avatarSrc}`);
+
+        let html = `
+            <h4 style="margin-left:5%;">${user}</h4>
+            <img src="${avatarSrc}" class="align-self-start mr-3" style="width:60px;margin-left:5%;">
+            <div class="media-body">
+              <p style="margin-left:5%;margin-top:1%;">Python: 30%</p>
+              <p style="margin-left:5%;">Language: --%</p>
+              <p style="margin-left:5%;">Language: --%</p>
+            </div>
+        `;
+
+        document.getElementById(id).innerHTML = html;
+        i += 1;
+    }
+}
+
