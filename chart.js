2f657f71f3a443a154d8de4424b7ac1553916cdd
Mass Kizar Cassiere <kcassiere97@csu.fullerton.edu>
2019-02-16 15:13:41 -0800

    Update 2,3 pages. Waiting for back end

---
+++ chart.js
@@ -0,0 +1,59 @@
+// Load Charts and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+
+google.charts.setOnLoadCallback(drawlanguageChart);
+
+
+google.charts.setOnLoadCallback(drawcommitChart);
+
+// Callback that draws the pie chart for languages
+function drawlanguageChart() {
+
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'languages');
+  data.addColumn('number', 'users');
+  data.addRows([
+    ['C++', 4],
+    ['Javascript', 2],
+    ['Python', 3],
+    ['C#', 1],
+    ['HTML', 4]
+  ]);
+
+  // Set options for language chart.
+  var options = {title:'Common Programming Language',
+                 width:800,
+                 height:800};
+
+  // Instantiate and draw the chart for languages.
+  var chart = new google.visualization.PieChart(document.getElementById('language_chart_div'));
+  chart.draw(data, options);
+}
+
+
+// Callback that draws the pie chart for repositories
+function drawcommitChart() {
+
+  // Create the data table for repositories
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'repositories');
+  data.addColumn('number', 'commits');
+  data.addRows([
+    ['A', 2],
+    ['B', 2],
+    ['C', 2],
+    ['D', 0],
+    ['D', 3]
+  ]);
+
+  // Set options for repositories pie chart.
+  var options = {title:'Who has more commits?',
+                 width:800,
+                 height:800};
+
+  // Instantiate and draw the chart for repositories
+  var chart = new google.visualization.PieChart(document.getElementById('repo_chart_div'));
+  chart.draw(data, options);
+}
