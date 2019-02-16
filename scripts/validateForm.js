d0ae21377ed0b33b2e3f070e1c3d100a093881a5
cbril <cbril@csu.fullerton.edu>
2019-02-16 13:34:14 -0800

    GitHub username and avatar are clickable links to the user's profile

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -20,7 +20,7 @@ if (users.length == 0){
     console.log("ERROR: no usernames were passed");
 
     let html = 
-    `<div id="error"
+    `<div class="error"
         <body>
             <h2>Error</h2>
             <p>Please enter at least one valid GitHub username.</p>
@@ -42,16 +42,17 @@ if (!errorCase){
 
     let i = 0;
     while(i < users.length){
-        let user = users[i];
-        console.log(`\tUser: ${user}`);
         let id = 'user' + (i+1);
-        console.log(`\tHTML id: ${id}`);
+        let user = users[i];
         let avatarSrc = "https://avatars3.githubusercontent.com/u/32445362?v=4";
-        console.log(`\tAvatar: ${avatarSrc}`);
+        let link = `https://github.com/${user}`;
 
         let html = `
-            <h4 style="margin-left:5%;">${user}</h4>
-            <img src="${avatarSrc}" class="align-self-start mr-3" style="width:60px;margin-left:5%;">
+            <h4 style="margin-left:5%;">
+              <a href=${link}>${user}</a></h4>
+            <a href=${link}>
+              <img src="${avatarSrc}" class="align-self-start mr-3" style="width:60px;margin-left:5%;">
+            </a>
             <div class="media-body">
               <p style="margin-left:5%;margin-top:1%;">Python: 30%</p>
               <p style="margin-left:5%;">Language: --%</p>
