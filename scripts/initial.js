9af7eaedaab852d09ce0505b82bca8be277c7565
Jacob Powell <jcbpowell12@gmail.com>
2019-02-16 14:33:39 -0800

    wrong user input creates html dom

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,19 +1,25 @@
 var API_BASE = "https://api.github.com";
 
+var initial_users = [];
+for(let i of [1,2,3,4]){
+    let param = 'user' + i;
+    let user = url.searchParams.get(param);
+    if(user){
+        initial_users.push(user);
+    }
+}
+verifyUsers();
+
 async function verifyUsers(){
   var cards = [];
-    var textfield = document.getElementsByClassName("username");
-    for(var i = 0; i < textfield.length; i++){
-      if(textfield[i].value != ''){
-        var data = await checkUsername(textfield[i].value)
+    for(var i = 0; i < users.length; i++){
+      if(users[i] != ''){
+        var data = await checkUsername(initial_users[i])
         if(data){
-          // console.log("Good User!");
           await cards.push(new userCard(data.login, data.email, data.avatar_url ));
-          
         }
       }
     }
-    console.log(cards);
 }
 
 
@@ -21,14 +27,36 @@ async function verifyUsers(){
 async function checkUsername(username){
   try {
     const API_ENDPOINT = `${API_BASE}/users/${username}`
-    console.log(API_ENDPOINT)
+    // console.log(API_ENDPOINT)
     const request = await fetch(API_ENDPOINT);
     const data = await request.json();
+    const status = await request.status;
+    if(status != 200){
+      throw {
+        statusCode: status,
+        userName: username
+      }
+    } else {
+      return data;
+    }
     return data;
   }
   catch (e) {
     console.log(e);
-    return false;
+    const detailError = `${e.statusCode} code received. ${e.userName} is an invalid github username`;
+    let html =
+    `<div id="error"
+          <body>
+              <h2 >Error</h2>
+              <p id="details">One or more of the GitHub usernames provided was invalid.</p>
+              <a href="index.html"><strong>Try again.</strong></a>
+          </body>
+      </div>`;
+    document.getElementById("submit-results").innerHTML = html;
+    let newDetail = document.createElement('p');
+    newDetail.textContent = detailError;
+    document.getElementById("details").append(newDetail);
+
   }
 }
 
